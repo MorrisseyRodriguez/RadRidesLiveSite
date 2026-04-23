d0187639f76184090b1323401933ec9373d024c2
MorrisseyRodriguez <mozr15294@gmail.com>
2026-04-23 10:12:57 -0700

    Start repository

---
+++ dist/assets/ui-CwY71_-a.js
@@ -0,0 +1,19 @@
+import{r as e}from"./vendor-R4cko80t.js";
+/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var r={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};
+/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=(t,a)=>{const h=e.forwardRef(({color:h="currentColor",size:o=24,strokeWidth:y=2,absoluteStrokeWidth:n,className:i="",children:s,...k},d)=>{return e.createElement("svg",{ref:d,...r,width:o,height:o,stroke:h,strokeWidth:n?24*Number(y)/Number(o):y,className:["lucide",`lucide-${l=t,l.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase()}`,i].join(" "),...k},[...a.map(([r,t])=>e.createElement(r,t)),...Array.isArray(s)?s:[s]]);var l});return h.displayName=`${t}`,h},a=t("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]),h=t("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]),o=t("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),y=t("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]),n=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]),i=t("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]),s=t("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]),k=t("Images",[["path",{d:"M18 22H4a2 2 0 0 1-2-2V6",key:"pblm9e"}],["path",{d:"m22 13-1.296-1.296a2.41 2.41 0 0 0-3.408 0L11 18",key:"nf6bnh"}],["circle",{cx:"12",cy:"8",r:"2",key:"1822b1"}],["rect",{width:"16",height:"16",x:"6",y:"2",rx:"2",key:"12espp"}]]),d=t("Instagram",[["rect",{width:"20",height:"20",x:"2",y:"2",rx:"5",ry:"5",key:"2e1cvw"}],["path",{d:"M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z",key:"9exkf1"}],["line",{x1:"17.5",x2:"17.51",y1:"6.5",y2:"6.5",key:"r4j83e"}]]),l=t("Mail",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]]),p=t("Menu",[["line",{x1:"4",x2:"20",y1:"12",y2:"12",key:"1e0a9i"}],["line",{x1:"4",x2:"20",y1:"6",y2:"6",key:"1owob3"}],["line",{x1:"4",x2:"20",y1:"18",y2:"18",key:"yk5zj1"}]]),c=t("Phone",[["path",{d:"M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z",key:"foiqr5"}]]),m=t("Send",[["path",{d:"m22 2-7 20-4-9-9-4Z",key:"1q3vgg"}],["path",{d:"M22 2 11 13",key:"nzbqef"}]]),f=t("Star",[["polygon",{points:"12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2",key:"8f66p6"}]]),g=t("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);
+/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */function x(e){var r,t,a="";if("string"==typeof e||"number"==typeof e)a+=e;else if("object"==typeof e)if(Array.isArray(e)){var h=e.length;for(r=0;r<h;r++)e[r]&&(t=x(e[r]))&&(a&&(a+=" "),a+=t)}else for(t in e)e[t]&&(a&&(a+=" "),a+=t);return a}function u(){for(var e,r,t=0,a="",h=arguments.length;t<h;t++)(e=arguments[t])&&(r=x(e))&&(a&&(a+=" "),a+=r);return a}export{a as A,y as C,d as I,l as M,c as P,f as S,g as X,h as a,n as b,m as c,k as d,p as e,u as f,i as g,o as h,s as i};
---
+++ src/main.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LogRocket from 'logrocket'
+
+import App from './App.jsx'
+import CarDetail from './components/CarDetail.jsx'
+import Layout from './components/Layout.jsx'
+import FAQ from './components/FAQ.jsx'
+import About from './components/About.jsx'
+import RequestCar from './components/RequestCar.jsx'
+import PrivacyPolicy from './components/PrivacyPolicy.jsx'
+import InsurancePolicy from './components/InsurancePolicy.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
+import { carsData } from './data/cars.jsx'
+import './index.css'
+
+// Initialize LogRocket
+LogRocket.init('vo2tda/analytics-for-rad-rides-bcr')
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <ScrollToTop />
+        <Layout />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/cars/:carId",
+        element: <CarDetail />,
+        loader: ({ params }) => {
+          const car = carsData[params.carId];
+          if (!car) {
+            throw new Error('Car not found');
+          }
+          return car;
+        },
+      },
+      {
+        path: "/faqs",
+        element: <FAQ />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/request-car",
+        element: <RequestCar />,
+      },
+      {
+        path: "/privacy",
+        element: <PrivacyPolicy />,
+      },
+      {
+        path: "/insurance-policy",
+        element: <InsurancePolicy />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
